dbf9bc11a7dd86e8a4035e2073f5d3a1766719b1
Akash kumar <akumar10@Academic.rrc.ca>
2025-03-17 13:43:15 -0500

    Added Java and JavaScript files for user input handling

---
+++ UserInput.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter your name: ", function(name) {
+    console.log("Hello, " + name + "!");
+    rl.close();
+});
